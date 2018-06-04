085273f7301f073eb9ab5f88f7a930df8ccdc5ae
maiao <39187220+maiao@users.noreply.github.com>
2018-06-04 23:33:35 +0200

    Add files via upload

--- next.js
+++ next.js
@@ -9,24 +9,30 @@ const bot = new Bot(
 });
 
 var tab = new Array();
-var compteur = 0;
 
 bot.respondTo('add ', (message, channel, user) =>
 {
 	message.text = message.text.replace('add ', '');
 	bot.send(`fonction qui ajoutera une branche au point principal et l'appelera ${message.text}`, channel)
-	var tmp = new Array(compteur, "null", message.text);
-	compteur++;
-	for(var i = 0; i < tab.length; i++)
+	var tmp;
+	if (tab.length > 0)
 	{
-		var tmp2 = tmp;
-		tmp = tab[i];
-		tab[i] = tmp2;
+		tmp = new Array("null", message.text, 1, tab[0][3]+1);
+		tab[0][0] = -1;
+		for(var i = 0; i < tab.length; i++)
+		{
+			var tmp2 = tmp;
+			tmp = tab[i];
+			tmp[0] = tmp[0] + 1;
+			tab[i] = tmp2;
+		}
+	}
+	else
+	{
+		tmp = new Array("null", message.text, 0, 0);
 	}
 	tab.push(tmp);
-	if(tab.length > 1)
-		tab[1][1] = tab[0][0];
-	for(var i = 0; i < tab.length; i++) bot.send(tab[i][0] + ' ; ' + tab[i][1] + ' ; ' + tab[i][2], channel);
+	for(var i = 0; i < tab.length; i++) bot.send(tab[i][0] + ' ; ' + tab[i][1] + ' ; ' + tab[i][2] + " ; " + tab[i][3], channel);
 }, true);
 
 bot.respondTo('addCurrent ', (message, channel, user) =>
@@ -42,7 +48,7 @@ bot.respondTo('addCurrent ', (message, channel, user) =>
 	var name = message.text.substring(0, nameBranche);
 	bot.send(`valeur recherchée : ${name}`, channel)
 	var i = 0;
-	while(i < tab.length && tab[i][2] != name)
+	while(i < tab.length && tab[i][1] != name)
 		i++;
 	if(i == tab.length)
 	{
@@ -56,26 +62,45 @@ bot.respondTo('addCurrent ', (message, channel, user) =>
 	bot.send(`insertion de ${message.text}`, channel)
 	if (i < tab.length-1) //pas le dernier élément
 	{
-		i++
+		tab[i][2]++; //enfantsDirects +1
+		tab[i][3]++; //enfantsTotaux +1
+		var parent = tab[i][0];
+		while(parent != "null" && parent > -1 && parent < tab.length) // +1 aux enfants totaux de tous les parents de cette branche
+		{
+			tab[parent][3] = tab[parent][3] + 1;
+			parent = tab[parent][0];
+		}
+		//i++;
 		var tmp = tab[i];
-		tab[i] = new Array(compteur, tab[i-1][0], message.text);
-		compteur++;
+		tab[i] = new Array(i, message.text, 0, 0); //nouvelle entrée
+		var position = i;
 		for (i; i < tab.length; i++)
 		{
+			if(tmp[0] > position)
+				tmp[0] = tmp[0] + 1;
 			var tmp2 = tmp;
 			tmp = tab[i];
 			tab[i] = tmp2;
 		}
+		if(tmp[0] > position)
+			tmp[0] = tmp[0] + 1;
 		tab.push(tmp);
 	}
 	else
 	{
-		tmp = new Array(compteur, tab[i][0], message.text);
-		compteur++;
+		tmp = new Array(i, message.text, 0, 0);
 		tab.push(tmp);
+		tab[i][2]++; //enfantsDirects +1
+		tab[i][3]++; //enfantsTotaux +1
+		var parent = tab[i][0];
+		while(parent != "null" && parent > -1 && parent < tab.length) // +1 aux enfants totaux de tous les parents de cette branche
+		{
+			tab[parent][3] = tab[parent][3] + 1;
+			parent = tab[parent][0];
+		}
 	}
 	
-	for(var i = 0; i < tab.length; i++) bot.send(tab[i][0] + ' ; ' + tab[i][1] + ' ; ' + tab[i][2], channel);
+	for(var i = 0; i < tab.length; i++) bot.send(tab[i][0] + ' ; ' + tab[i][1] + ' ; ' + tab[i][2] + " ; " + tab[i][3], channel); //pour le test
 	
 	/*// get the arguments from the message body
 	let args = getArgs(message.text);
@@ -117,6 +142,79 @@ bot.respondTo('afficher', (message, channel, user) =>
 	if(tab.length < 1)
 		bot.send('rien à afficher', channel);
 }, true);
+
+bot.respondTo('algo', (message, channel, user) => //le tableau devrait être dans l'ordre et ne pas avoir
+{
+	algo(message, channel, 0, 0)
+	/*var tailleEspacement = 0;
+	var coté = 0;
+	if(tab[i][2] > tab[i][3]) // donner une taille sur le nombre d'enfants directs au noeud
+	{
+		tailleEspacement = tab[i][2]*100;
+	}
+	else // enfants indirects s'ils y en a plus
+	{
+		tailleEspacement = tab[i][3]*100;
+	}
+	coté = (tailleEspacement/2) - (tailleEspacement/2)%100; //cas impair le milieu sera à 0
+	
+	for(var j = i+1; j < tab.length; j++) // rechercher tous les enfants du noeud pour les traiter, ils ne seront pas traité d'une autre manière
+	{
+		if(tab[j][0] == i)
+		{
+			algo(message, channel, coté, j);
+			coté -=100;
+		}
+	}*/
+	
+	//for(var j = i; j < tab.length; j++) //ne sera pas fait à la fin car on utilisera une fonction recursive, c'est plus facile à comprendre comme ça et je ne sais pas faire de fonction en javascript
+	
+	//bot.send('noeud : ' + tab[i][1] + 'largeur de la ligne : ' + tailleEspacement, channel);
+}, true);
+
+function algo(message/*optionel, c'est pour l'affichage*/, channel/*pour l'affichage aussi*/, coté, i)
+{// positionner ce noeud, appeler ses enfants
+	if(i >= tab.length)
+		return;
+	var tailleEspacement = 0;
+	
+	if(tab[i][2] >= (tab[i][3] - tab[i][2]) || tab[i][2] == 1) // donner une taille sur le nombre d'enfants directs au noeud
+	{
+		tailleEspacement = tab[i][2]*100;
+	}
+	else // enfants indirects s'ils y en a plus
+	{
+		tailleEspacement = (tab[i][3]-tab[i][2])*100;
+	}
+	
+	if(tab[i][0] != 'null')
+		bot.send('noeud : ' + tab[i][1] + '; positionné par rapport au noeud précédent : ' + coté + '; (parent ' + tab[tab[i][0]][1] + ')', channel);
+	else
+		bot.send('noeud : ' + tab[i][1] + '; positionné par rapport au noeud précédent : ' + coté + '; (parent null)', channel);
+	
+	coté = (tailleEspacement/2) - (tailleEspacement/2)%100; //cas impair le milieu sera à 0
+	
+	var test = 0;
+	for(var j = i+1; j < tab.length; j++) // rechercher tous les enfants du noeud pour les traiter, ils ne seront pas traité d'une autre manière
+	{
+		if(tab[j][0] == i)
+		{
+			if (test != 0)
+			{
+				coté -=100*(tab[j][3] + 1);
+				algo(message, channel, coté, j);
+			}
+			else
+			{
+				algo(message, channel, coté, j);
+				coté -=100*(tab[j][3] + 1);
+			}
+			if (coté == 0 && tailleEspacement%200 == 0) // cas pair on ne doit rien mettre au milieu
+				coté -= 100;
+		}
+		test++;
+	}
+}
 
 function getName(id)
 {
